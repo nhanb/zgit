25afbe6749bbc436f646ee05da17aa232759d3cc
Nhân <hi@imnhan.com>
2024-10-19 16:02:40 +0700

    install httpz

--- build.zig
+++ build.zig
@@ -22,6 +22,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const httpz = b.dependency("httpz", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.root_module.addImport("httpz", httpz.module("httpz"));
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,28 @@
 const std = @import("std");
+const httpz = @import("httpz");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // More advance cases will use a custom "Handler" instead of "void".
+    // The last parameter is our handler instance, since we have a "void"
+    // handler, we passed a void ({}) value.
+    var server = try httpz.Server(void).init(allocator, .{ .port = 5882 }, {});
+    defer {
+        // clean shutdown, finishes serving any live request
+        server.stop();
+        server.deinit();
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var router = server.router(.{});
+    router.get("/api/user/:id", getUser, .{});
 
-    try bw.flush(); // don't forget to flush!
+    // blocks
+    try server.listen();
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn getUser(req: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    try res.json(.{ .id = req.param("id").?, .name = "Teg" }, .{});
 }
