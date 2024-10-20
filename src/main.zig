e654f36659d4789e08c69250388e16c435b5015b
Nhân <hi@imnhan.com>
2024-10-20 16:08:18 +0700

    use html.zig for Register page

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,22 @@
 const std = @import("std");
 const httpz = @import("httpz");
 const zqlite = @import("zqlite");
+const html = @import("./html.zig");
 
-const DB_PATH: [*:0]const u8 = "workdir/zgit.db";
+const DB_PATH = "workdir/zgit.db";
+const PORT = 8000;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
     try initDb(DB_PATH);
-    std.debug.print("Initialized {s}.\n", .{DB_PATH});
+    std.debug.print("Initialized {s}\n", .{DB_PATH});
 
     // More advance cases will use a custom "Handler" instead of "void".
     // The last parameter is our handler instance, since we have a "void"
     // handler, we passed a void ({}) value.
-    var server = try httpz.Server(void).init(allocator, .{ .port = 5882 }, {});
+    var server = try httpz.Server(void).init(allocator, .{ .port = PORT }, {});
     defer {
         // clean shutdown, finishes serving any live request
         server.stop();
@@ -22,31 +24,95 @@ pub fn main() !void {
     }
 
     var router = server.router(.{});
-    router.get("/api/user/:id", getUser, .{});
-    std.debug.print("Started server.\n", .{});
+    router.get("/register", handleRegister, .{});
+    router.get("/static/style.css", handleStyleCss, .{});
+    router.get("/static/register.css", handleRegisterCss, .{});
+    std.debug.print("Started server at port {d}\n", .{PORT});
 
     // blocks
     try server.listen();
 }
 
-fn getUser(req: *httpz.Request, res: *httpz.Response) !void {
+fn handleStyleCss(_: *httpz.Request, res: *httpz.Response) !void {
     res.status = 200;
-    try res.json(.{ .id = req.param("id").?, .name = "Teg" }, .{});
+    res.body = @embedFile("static/style.css");
 }
 
-fn initDb(db_path: [*:0]const u8) !void {
+fn handleRegisterCss(_: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    res.body = @embedFile("static/register.css");
+}
+
+fn handleRegister(_: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    const h = html.Builder{ .allocator = res.arena };
+    const body = h.html(
+        .{ .lang = "en" },
+        .{
+            h.head(null, .{
+                h.title(null, .{"Register | zgit"}),
+                h.link(.{ .rel = "stylesheet", .href = "/static/style.css" }),
+                h.link(.{ .rel = "stylesheet", .href = "/static/register.css" }),
+            }),
+            h.body(null, .{
+                h.h1(null, .{"Register"}),
+                h.form(
+                    .{ .style = "max-width: 30rem" },
+                    .{
+                        h.label(
+                            .{ .@"for" = "username" },
+                            .{"Username:"},
+                        ),
+                        h.input(
+                            .{
+                                .type = "text",
+                                .id = "username",
+                                .name = "username",
+                                .required = "",
+                            },
+                        ),
+                        h.label(
+                            .{ .@"for" = "password" },
+                            .{"Password:"},
+                        ),
+                        h.input(
+                            .{
+                                .type = "text",
+                                .id = "password",
+                                .name = "password",
+                                .required = "",
+                            },
+                        ),
+                        h.input(
+                            .{
+                                .type = "submit",
+                                .value = "Register",
+                            },
+                        ),
+                    },
+                ),
+            }),
+        },
+    );
+
+    const writer = res.writer();
+    try h.writeDoctype(writer);
+    try body.writeTo(writer);
+}
+
+fn initDb(db_path: [:0]const u8) !void {
     const flags = zqlite.OpenFlags.Create | zqlite.OpenFlags.EXResCode;
     var conn = try zqlite.open(db_path, flags);
     defer conn.close();
 
-    try conn.exec(
+    try conn.execNoArgs(
         \\create table users (
         \\    id integer primary key,
         \\    username text unique not null
         \\);
-    , .{});
+    );
 
-    try conn.exec(
+    try conn.execNoArgs(
         \\create table repo (
         \\    id integer primary key,
         \\    name text unique not null,
@@ -55,9 +121,9 @@ fn initDb(db_path: [*:0]const u8) !void {
         \\
         \\    foreign key (created_by) references users (id)
         \\);
-    , .{});
+    );
 
-    try conn.exec(
+    try conn.execNoArgs(
         \\create table user_repo_access (
         \\    user_id integer not null,
         \\    repo_id integer not null,
@@ -67,5 +133,5 @@ fn initDb(db_path: [*:0]const u8) !void {
         \\    foreign key (user_id) references users (id),
         \\    foreign key (repo_id) references repo (id)
         \\);
-    , .{});
+    );
 }
